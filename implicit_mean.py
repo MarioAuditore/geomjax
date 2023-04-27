Instability appeared in mean calculation. Tried to fix it by introducing random generators for manifolds and gradient clipping. Also made momentum optimiser formula more accurate

implicit_mean.py
@@ -30,7 +30,7 @@ import matplotlib.pyplot as plt
 
 
 # @implicit_diff.custom_root(grad(pairwise_distance))
-def gradient_descend_weighted_mean(X_set, weights, optimiser, plot_loss_flag, maxiter):
+def gradient_descend_weighted_mean(X_set, weights, optimiser, plot_loss_flag, maxiter, debug = False):
     """
     Weighted mean calculation as an optimization problem:
     find point, which minimises pairwise distances in the given set of points
@@ -45,19 +45,37 @@ def gradient_descend_weighted_mean(X_set, weights, optimiser, plot_loss_flag, ma
     if plot_loss_flag:
         plot_loss = []
 
+    key,_ = random.split(random.PRNGKey(0))
     # init mean with random element from set and move it a bit
-    Y = X_set[np.random.randint(0, X_set.shape[0], (1,))][0] + 1e-7
+    if optimiser.manifold.random_generator is None:
+        if len(X_set.shape) > 2:
+            Y = X_set[np.random.randint(0, X_set.shape[0], (1,))][0] + jnp.abs(random.uniform(key, shape=(X_set.shape[-1],)) * 1e-7)
+        else:
+            Y = X_set[np.random.randint(0, X_set.shape[0], (1,))][0] + 1e-7
+    else:
+        Y = optimiser.manifold.generate(X_set.shape[-2], X_set.shape[-1])
 
-    momentum = None
+    optim_state = None
 
     for i in range(maxiter):
 
         # calculate loss
         loss = pairwise_distance(Y, X_set, optimiser.manifold.distance, weights)
 
+        if debug:
+            print(f"Iter {i} | loss = {loss}")
+
         euclid_grad = grad(pairwise_distance, argnums=0)(Y, X_set, optimiser.manifold.distance, weights)
 
-        Y, momentum = optimiser.step(Y, euclid_grad, momentum)
+        euclid_grad = euclid_grad / jnp.linalg.norm(euclid_grad)
+
+        if debug:
+            print(f"Euclid grad norm:{jnp.linalg.norm(euclid_grad)}")
+
+        Y, optim_state = optimiser.step(Y, euclid_grad, optim_state)
+
+        if debug:
+            print(f"Updated mean: {jnp.linalg.eigh(Y)[0]}")
 
         if plot_loss_flag:
             # collect loss for plotting
@@ -131,22 +149,23 @@ def weighted_mean_implicit_matrix_derivative(x, X, w, manifold):
 
 
 
-@partial(custom_vjp, nondiff_argnums=(2, 3, 4))
-def weighted_mean(X, w, optimiser, plot_loss_flag=False, maxiter=200):
+@partial(custom_vjp, nondiff_argnums=(2, 3, 4, 5))
+def weighted_mean(X, w, optimiser, plot_loss_flag=False, maxiter=200, debug = False):
     return gradient_descend_weighted_mean(X_set=X, 
                                           weights=w, 
                                           optimiser=optimiser, 
                                           plot_loss_flag=plot_loss_flag,
-                                          maxiter=maxiter)
+                                          maxiter=maxiter,
+                                          debug=debug)
 
 
-def weighted_mean_fwd(X, w, optimiser, plot_loss_flag, maxiter):
+def weighted_mean_fwd(X, w, optimiser, plot_loss_flag, maxiter, debug):
     # Returns primal output and residuals to be used in backward pass by f_bwd.
-    x = weighted_mean(X, w, optimiser, plot_loss_flag, maxiter)
+    x = weighted_mean(X, w, optimiser, plot_loss_flag, maxiter, debug)
     return x, (x, X, w, optimiser.manifold)
 
 
-def weighted_mean_bwd(optimiser, plot_loss_flag, maxiter, res, g):
+def weighted_mean_bwd(optimiser, plot_loss_flag, maxiter, debug, res, g):
     x, X, w, manifold = res  # Gets residuals computed in f_fwd
     if len(x.shape) > 1:
         grad_X, grad_w = weighted_mean_implicit_matrix_derivative(x, X, w, manifold)

manifolds/stiefel.py
@@ -11,19 +11,20 @@ from jax import tree_util, random
 # Base of math operations and derivatives
 from jax import numpy as jnp
 # 
-import numpy as np
+from jax import random
 # General functions for manifolds
 from geomjax.manifolds.utils import Manifold
 
 
 
-def generate_orthogonal(n, m):
+def generate_orthogonal(key, n, m):
+    
     if n >= m:
-        Q,_ = jnp.linalg.qr(np.random.randn(n, n))
+        Q,_ = jnp.linalg.qr(random.uniform(key, shape=(n, n)))
         Q = Q[:m, :]
         return Q.T
     else:
-        Q,_ = jnp.linalg.qr(np.random.randn(m, m))
+        Q,_ = jnp.linalg.qr(random.uniform(key, shape=(m, m)))
         Q = Q[:n, :]
         return Q
 
@@ -88,10 +89,13 @@ class Stiefel(Manifold):
     """
     SPD - manifold of symmetric positive definite matrices
     """
-    def __init__(self, projection = projection_1, retraction = retraction_svd, distance = distance):
+    def __init__(self, projection = projection_1, retraction = retraction_svd, distance = distance, random_generator = generate_orthogonal):
         self.projection = projection
         self.retraction = retraction
         self.distance = distance
+
+        self.key = random.PRNGKey(1234)
+        self.random_generator = random_generator
 
 
 tree_util.register_pytree_node(Stiefel,

models/spdnet.py
@@ -66,7 +66,6 @@ class MultiMapLayer(nn.Module):
             oper_2 = vmap(lambda X, w: X @ w, (0, None), 0)
             return oper_2(oper_1(w, X), w)
         
-        gen_key = random.PRNGKey(0)
         submanifold_maps = self.param('Matrix',
                                     self.params_init, # Initialization function for Orthogonal matrix
                                     self.n_submanifolds, inputs.shape[-1], self.out_dim)  # shape info.
@@ -77,14 +76,16 @@ class MultiMapLayer(nn.Module):
         return y
 
 
+
 class ReEigLayer(nn.Module):
-    threschold: int
+    threschold: int = 1e-5
 
     @nn.compact
     def __call__(self, inputs):
+        
         def reeig(M):
             evals, evecs = jnp.linalg.eigh(M)
-            evals = jnp.maximum(evals, self.threschold)
+            new_evals = jnp.maximum(evals, self.threschold)
             return evecs @ jnp.diag(evals) @ evecs.T
         
         if len(inputs.shape) > 2:
@@ -129,6 +130,7 @@ class Triu(nn.Module):
 class SPDAvgPooling(nn.Module):
     optimiser: Any
     maxiter: int = 100
+    debug: bool = False
     weights_init: Callable = nn.initializers.uniform()
 
     @nn.compact
@@ -136,7 +138,7 @@ class SPDAvgPooling(nn.Module):
         weights = self.param('weights', self.weights_init, (inputs.shape[-3], ))  # shape info.
         if len(inputs.shape) > 3:
             def vectorized(inputs):
-                return weighted_mean(inputs, weights, self.optimiser, maxiter=self.maxiter)
+                return weighted_mean(inputs, weights, self.optimiser, maxiter=self.maxiter, debug=self.debug)
             y = vmap(vectorized)(inputs)
         else:
             y = weighted_mean(inputs, weights, self.optimiser, maxiter=self.maxiter)