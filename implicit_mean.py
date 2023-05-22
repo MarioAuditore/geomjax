fixed pmap in derivative

implicit_mean.py
@@ -147,24 +147,24 @@ def weighted_mean_implicit_matrix_derivative(x, X, w, manifold):
     # Vectorize
     grad_multiply_inverse_batch = vmap(grad_multiply_inverse, (None, 0), 0)
     # Find suitable for pmap amount of cores
-    # n_jobs = calc_n_jobs(X.shape[0])
+    n_jobs = calc_n_jobs(X.shape[0])
 
     # If there is no parallelisation option
-    # if n_jobs == 1:
-    dfdx = grad_multiply_inverse_batch(d2yy_inv, d2xy)
-    dfdw = grad_multiply_inverse_batch(d2yy_inv, d2wy)
-    # else:
-    #     # print(f"DEBUG: Derivative in parallel mode on {n_jobs} devices")
-    #     grad_multiply_inverse_batch_parallel = pmap(grad_multiply_inverse_batch, in_axes=(None, 0), out_axes=0)
-    #     # Split by jobs
-    #     d2xy_batched = parallelize_array(d2xy, n_jobs)
-    #     d2wy_batched = parallelize_array(d2wy, n_jobs)
-    #     # Parallel computation
-    #     dfdx_batched = grad_multiply_inverse_batch_parallel(d2yy_inv, d2xy_batched)
-    #     dfdw_batched = grad_multiply_inverse_batch_parallel(d2yy_inv, d2wy_batched)
-    #     # Merge results
-    #     dfdx = merge_parallel_results(dfdx_batched)
-    #     dfdw = merge_parallel_results(dfdw_batched)
+    if n_jobs == 1:
+        dfdx = grad_multiply_inverse_batch(d2yy_inv, d2xy)
+        dfdw = grad_multiply_inverse_batch(d2yy_inv, d2wy)
+    else:
+        # print(f"DEBUG: Derivative in parallel mode on {n_jobs} devices")
+        grad_multiply_inverse_batch_parallel = pmap(grad_multiply_inverse_batch, in_axes=(None, 0), out_axes=0)
+        # Split by jobs
+        d2xy_batched = parallelize_array(d2xy, n_jobs)
+        d2wy_batched = parallelize_array(d2wy, n_jobs)
+        # Parallel computation
+        dfdx_batched = grad_multiply_inverse_batch_parallel(d2yy_inv, d2xy_batched)
+        dfdw_batched = grad_multiply_inverse_batch_parallel(d2yy_inv, d2wy_batched)
+        # Merge results
+        dfdx = merge_parallel_results(dfdx_batched)
+        dfdw = merge_parallel_results(dfdw_batched)
 
     return dfdx, dfdw
 
@@ -189,25 +189,7 @@ def weighted_mean_fwd(X, w, optimiser, plot_loss_flag, maxiter, debug):
 def weighted_mean_bwd(optimiser, plot_loss_flag, maxiter, debug, res, g):
     x, X, w, manifold = res  # Gets residuals computed in f_fwd
     if len(x.shape) > 1:
-        n_jobs = calc_n_jobs(X.shape[0])
-        if n_jobs == 1:
-            grad_X, grad_w = weighted_mean_implicit_matrix_derivative(x, X, w, manifold)
-        else:
-            # print(f"DEBUG: Derivative in parallel mode on {n_jobs} devices")
-            # Make parallel versiob
-            weighted_mean_implicit_matrix_derivative_parallel = pmap(
-                lambda X, w: weighted_mean_implicit_matrix_derivative(x, X, w, manifold),
-                in_axes=(0, 0), 
-                out_axes=0)
-            # Split arrays by devices
-            X_batched = parallelize_array(X, n_jobs)
-            w_batched = parallelize_array(w, n_jobs)
-            # Compute
-            grad_X_batched, grad_w_batched = weighted_mean_implicit_matrix_derivative_parallel(X_batched, w_batched)
-            # Merge results
-            grad_X = merge_parallel_results(grad_X_batched)
-            grad_w = merge_parallel_results(grad_w_batched)
-
+        grad_X, grad_w = weighted_mean_implicit_matrix_derivative(x, X, w, manifold)
         out_X = grad_X @ g
         out_w = jnp.trace(grad_w @ g, axis1=-2, axis2=-1)
         return (out_X, out_w)

manifolds/spd.py
@@ -12,6 +12,7 @@ from jax import tree_util, lax
 # Base of math operations and derivatives
 from jax import numpy as jnp
 from jax import scipy as sp
+from jax import jit
 # General functions for manifolds
 from geomjax.manifolds.utils import Manifold
 from sklearn.datasets import make_spd_matrix
@@ -22,6 +23,7 @@ from sklearn.datasets import make_spd_matrix
 
 # Implementation of matrix fractional power for pytorch
 # https://discuss.pytorch.org/t/raising-a-tensor-to-a-fractional-power/93655/3
+@jit
 def matrix_fractional_pow(M, power):
     evals, evecs = jnp.linalg.eigh(M)
     evpow = evals ** power
@@ -29,6 +31,7 @@ def matrix_fractional_pow(M, power):
 
 
 # Matrix logarithm based on same idea as above
+@jit
 def matrix_log(M):
     evals, evecs = jnp.linalg.eigh(M)
     # to prevent from zeros in log
@@ -36,7 +39,7 @@ def matrix_log(M):
     return evecs @ jnp.diag(evlog) @ evecs.T
     
 
-
+@jit
 def projection(M, S):
     """
     Projection from ambient space to tangent space at x
@@ -46,6 +49,7 @@ def projection(M, S):
     return M @ (S + S.T) @ M / 2
 
 
+@jit
 def retraction(M, T):
     """
     Retraction from tangent space to manifold
@@ -60,18 +64,18 @@ def retraction(M, T):
 
     return M_half @ exp_res @ M_half
 
-
+@jit
 def affine_invariant_metric(X, Y):    
     X_neg_half = matrix_fractional_pow(X, -0.5)
     return jnp.linalg.norm(matrix_log(X_neg_half @ Y @ X_neg_half))
 
-
+@jit
 def stein_metric(X, Y):
     det_1 = jnp.linalg.det((X + Y) / 2)
     det_2 = jnp.linalg.det(X @ Y)
     return jnp.log(det_1) - jnp.log(det_2) / 2
 
-
+@jit
 def log_euclidean_metric(X, Y):
     log_X = matrix_log(X)
     log_Y = matrix_log(Y)