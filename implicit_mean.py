a02e43b11f480e2794b5b96f0db1b1da5fe86447
MarioAuditore <ersabitov@miem.hse.ru>
2023-05-24 10:51:24 +0300

    Simple init of mean... again, because clever init seems to be slow

--- implicit_mean.py
+++ implicit_mean.py
@@ -46,17 +46,22 @@ def gradient_descend_weighted_mean(X_set, weights, optimiser, plot_loss_flag, ma
     # Set randomness
     key,_ = random.split(random.PRNGKey(0))
     # Choose a subset
-    if X_set.shape[0] > 5:
-        X_sample = random.choice(key, X_set, shape=(5,))
+    # if X_set.shape[0] > 5:
+    #     X_sample = random.choice(key, X_set, shape=(5,))
+    # else:
+    #     X_sample = X_set
+    # # Find pairwise distances for each point in a subset
+    # distances = vmap(pairwise_distance, (0, None, None), 0)(X_sample, X_set, optimiser.manifold.distance)
+    # # Init mean with a point closest to other points
+    # if len(X_set.shape) > 2:
+    #     Y = X_set[jnp.argmin(distances)] + jnp.abs(random.uniform(key, shape=(X_set.shape[-1],)) * 1e-4)
+    # else:
+    #     Y = X_set[jnp.argmin(distances)] + 1e-4
+
+    f len(X_set.shape) > 2:
+        Y = X_set[random.choice(key, X_set, shape=(1,))[0]] + jnp.abs(random.uniform(key, shape=(X_set.shape[-1],)) * 1e-5)
     else:
-        X_sample = X_set
-    # Find pairwise distances for each point in a subset
-    distances = vmap(pairwise_distance, (0, None, None), 0)(X_sample, X_set, optimiser.manifold.distance)
-    # Init mean with a point closest to other points
-    if len(X_set.shape) > 2:
-        Y = X_set[jnp.argmin(distances)] + jnp.abs(random.uniform(key, shape=(X_set.shape[-1],)) * 1e-4)
-    else:
-        Y = X_set[jnp.argmin(distances)] + 1e-4
+        Y = X_set[random.choice(key, X_set, shape=(1,))[0]] + 1e-4
 
     optim_state = optimiser.init(Y)
 
@@ -102,7 +107,7 @@ def gradient_descend_weighted_mean(X_set, weights, optimiser, plot_loss_flag, ma
 
 # === Functions for mean derivative ===
 # Multiplication
-# @jit
+@jit
 def grad_multiply_inverse(dyy_inv, d_mixed):
         return -dyy_inv @ d_mixed
 
